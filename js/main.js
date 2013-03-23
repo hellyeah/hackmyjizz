0925c8a9ef7031480ef6a15c07e6886cdcea1515
gailees <gailees@umich.edu>
2013-03-23 14:35:19 -0400

    isotope not showing up

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-//Written by Jesse Daugherty and Raj Vir
-//(hop off our source code)
-
 $(document).ready(function(){
 
 $('.deals-container').isotope({
@@ -10,7 +7,7 @@ $('.deals-container').isotope({
   	masonry : { 
    		columnWidth : 320 
   	}
-});	
+});
 
 Parse.initialize("78XHwuLO8Zzh69s6LJjLPMrysLrJrlMIXhZFrWA1", "yI38JPljByHBEVZcE4B5n51gpQ5Fh9JrSoQMUu78");
 var deal_template_source = $('#deal-template').html(),
@@ -19,59 +16,29 @@ var deal_template_source = $('#deal-template').html(),
 	dropdown_item_template = Handlebars.compile(dropdown_template_source),
 	imgUrl = "http://www.photo-dictionary.com/photofiles/list/8/667airplane.jpg";
 
-var DONORS = DONORS || {};
+var TRIP = TRIP || {};
 
-//mixpanel.track('Loaded Page');
-
-$.extend(DONORS, {
-	user_airport: "DTW",
+$.extend(TRIP, {
+	
 	item_count: 0,
-	getURLParameter: function (name) {
-	    return decodeURI(
-	        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
-	    );
-	},
-	locationString: function(str) {
-		var parts = str.split(",");
-		if (parts) {
-			if (parts[1] != " DC") return parts[0];
-		}
-		return str;
-	},
-	setLocale: function() {
-		var param = TRIP.getURLParameter("origin");
-		if (param != 'null') TRIP.user_airport = param.substring(0, param.length - 1);;
-		var Cities = Parse.Object.extend("Cities"),
-		city = new Cities();
-		query = new Parse.Query(Cities);
-		query.equalTo("airport_code", TRIP.user_airport);
-		query.find({
-			success: function(results) {
-				if (results && results.length > 0)
-					$('#dropdown-button').prepend(results[0].get("city") + ", " + results[0].get("state") + " (" + results[0].get("airport_code") + ") ");
-			},
-			error: function(results) {
-			    alert("Error: " + error.code + " " + error.message);
-			}
-		})
-	},
+	/*
 	appendDeal: function(flight, styleName) {
-		var aptCode = flight.get("destCode"),
-		CityImages = Parse.Object.extend("CityImages"),
-		cityImage = new CityImages(),
-		cityQuery = new Parse.Query(CityImages);
-      	cityQuery.equalTo("airportCode", aptCode);
+		//var aptCode = flight.get("destCode"),
+		//CityImages = Parse.Object.extend("CityImages"),
+		//cityImage = new CityImages(),
+		//cityQuery = new Parse.Query(CityImages);
+      	//cityQuery.equalTo("airportCode", aptCode);
       	cityQuery.first({
 		  success: function(result) {
-		  	imgUrl = result.get("imageUrl");
-			var startDate =  moment(flight.get("departDate"), "MM/DD/YY"),
-			returnDate = moment(flight.get("returnDate"), "MM/DD/YY"),
-			numNights = returnDate.diff(startDate,'days'),
-			dafact = ((Math.random() * 1.2) + 1.4),
+		  	imgUrl = result.get("imgUrl");
+			//var startDate =  moment(flight.get("departDate"), "MM/DD/YY"),
+			//returnDate = moment(flight.get("returnDate"), "MM/DD/YY"),
+			//numNights = returnDate.diff(startDate,'days'),
+			//dafact = ((Math.random() * 1.2) + 1.4),
 			flightPrice = parseInt(flight.get("price")),
 			hotelPrice = flight.get("hotel_price");
 			if (hotelPrice == null) hotelPrice = 75; 
-			
+
 			var totalPrice = (hotelPrice * numNights) + flightPrice;
 		  	var html = deal_template({
 				destination: TRIP.locationString(flight.get("destLocation")), 
@@ -87,8 +54,8 @@ $.extend(DONORS, {
 			})
 			$('.deal-box').first().addClass('large');
 			$('.deals-container').isotope('insert', $(html));
-			DONORS.item_count++;
-			if (DONORS.item_count == DONORS.numCities) {
+			TRIP.item_count++;
+			if (TRIP.item_count == TRIP.numCities) {
 				$('.deal-box').click(function(){
 					mixpanel.track('Enlarged Deal', $(this).find('.destination-name').text());
 				 	$('.deal-box.large').removeClass('large');
@@ -96,8 +63,7 @@ $.extend(DONORS, {
 				 	$('.deals-container').isotope('reLayout');
 			 	});			
 			}
-			mixpanel.track_links(".buynow", "Click Buynow", function(ele) { return { type: $(ele).parent().find('.destination-name')}});
-
+			
 		  },
 		  error: function(error) {
 		    alert("Error: " + error.code + " " + error.message);
@@ -105,46 +71,101 @@ $.extend(DONORS, {
 		});
 
 	},
+	*/
 	getDeals: function() {
-		var Deals = Parse.Object.extend("Deals"),
+		var Deals = Parse.Object.extend("Donor"),
 		deal = new Deals(),
 		query = new Parse.Query(Deals);
-		query.equalTo("originCode", DONORS.user_airport);
-		query.ascending("departDate");
 		query.count({
 			success:function(count) {
-				DONORS.numCities = count;
+				TRIP.numCities = count;
 			},
-			error: function(result) {
+				error: function(result) {
 				//do nothing
 			}
 		});
 		query.find({
 		  success: function(results) {
 		  	$.each(results, function() {
-		  		DONORS.appendDeal(this);
-		  	});
+		  		console.log(this);
+		  		console.log(this.get("Accomplishments"));
+		  		//TRIP.appendDeal(this);
 
+		  		//Compiling html with handlebars.js
+		  		var html = deal_template({
+				destination: TRIP.locationString(flight.get("destLocation")), 
+				bg: this.get("imgUrl"),
+				trip_length: this.get("Accomplishments"),
+				//leaving: startDate.calendar(),
+				price: this.get("price"),
+				//old_price: this.get("price"),
+				//hotel_price: Math.round(hotelPrice),
+				total_price: this.get("price"),
+				buynowhref: this.get("imgUrl"),
+				//booknowhref: flight.get("hotel_link")
+			})
+			$('.deal-box').first().addClass('large');
+			$('.deals-container').isotope('insert', $(html));
+			TRIP.item_count++;
+			if (TRIP.item_count == TRIP.numCities) {
+				$('.deal-box').click(function(){
+				 	$('.deal-box.large').removeClass('large');
+				 	$(this).addClass('large');
+				 	$('.deals-container').isotope('reLayout');
+			 	});			
+			}
+		  	});
 		  },
 		  error: function(error) {
 		    alert("Error: " + error.code + " " + error.message);
 		  }
 		});
 	},	
-	getCities: function() {		
-  		origin_cities.sort(function(a,b) {return (a.city < b.city) ? -1 : 1;})
-  		$.each(origin_cities, function() {
-			var html = dropdown_item_template({
-				airport_code: this.airport_code, 
-				city: this.city,
-				state: this.state
-			});
-			$("#dropdown-cities ul").append(html);
-		});
-	}
 });
- DONORS.setLocale();
- DONORS.getDeals();
- DONORS.getCities();
+ TRIP.getDeals();
+
+//mixpanel.track('Loaded Page');
+/*
+	function appendListing(html){
+	    var bound = $(html).click(function(){
+	      analytics.track("Enlarged Brick");
+	      //analytics.track("Enlarged Brick " + $(this).find('.title').text());
+	      $('.brick.large').removeClass('large');
+	      $(this).addClass('large');
+	      $('.brick-container').isotope('reLayout');
+	    })
+	    setTimeout(function(){
+	      $('.brick-container').isotope('insert', bound);
+	      $('.brick-container').isotope('reLayout')
+	      render();
+	    },0)
+	};
+
+	var ListingsQuery = new Parse.Query(Donor);
+	ListingsQuery._order = "-updatedAt";
+
+	var skip = 0;
+	var count = 40;
+	ListingsQuery.limit(count);
+
+	function loadListings(){
+	    ListingsQuery.find({ 
+	      success: function(results){
+	        $.each(results, function(i, item){
+	            var compiled = _.template($('.listing-template').html());
+	            html = compiled({
+	              l: item,
+	            })
+	            appendListing(html);
+	        })
+	        skip += count;
+	        ListingsQuery.skip(skip);
+	      }
+	    })    
+	  };
+*/
+
+  	//loadListings();
+
 
 });	
