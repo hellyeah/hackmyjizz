90b46b4e59218b44c7feb1b11e650ed4ef9f54c7
gailees <gailees@umich.edu>
2013-03-23 17:06:12 -0400

    fixed clicks

--- js/main.js
+++ js/main.js
@@ -21,57 +21,7 @@ var TRIP = TRIP || {};
 $.extend(TRIP, {
 	
 	item_count: 0,
-	/*
-	appendDeal: function(flight, styleName) {
-		//var aptCode = flight.get("destCode"),
-		//CityImages = Parse.Object.extend("CityImages"),
-		//cityImage = new CityImages(),
-		//cityQuery = new Parse.Query(CityImages);
-      	//cityQuery.equalTo("airportCode", aptCode);
-      	cityQuery.first({
-		  success: function(result) {
-		  	imgUrl = result.get("imgUrl");
-			//var startDate =  moment(flight.get("departDate"), "MM/DD/YY"),
-			//returnDate = moment(flight.get("returnDate"), "MM/DD/YY"),
-			//numNights = returnDate.diff(startDate,'days'),
-			//dafact = ((Math.random() * 1.2) + 1.4),
-			flightPrice = parseInt(flight.get("price")),
-			hotelPrice = flight.get("hotel_price");
-			if (hotelPrice == null) hotelPrice = 75; 
 
-			var totalPrice = (hotelPrice * numNights) + flightPrice;
-		  	var html = deal_template({
-				destination: TRIP.locationString(flight.get("destLocation")), 
-				bg: imgUrl,
-				trip_length: "for " + numNights + ((numNights != 1) ? " Nights" : " Night"),
-				leaving: startDate.calendar(),
-				price: Math.round(flightPrice),
-				old_price: parseInt(dafact * flightPrice),
-				hotel_price: Math.round(hotelPrice),
-				total_price: Math.round(totalPrice),
-				buynowhref: flight.get("link"),
-				booknowhref: flight.get("hotel_link")
-			})
-			$('.deal-box').first().addClass('large');
-			$('.deals-container').isotope('insert', $(html));
-			TRIP.item_count++;
-			if (TRIP.item_count == TRIP.numCities) {
-				$('.deal-box').click(function(){
-					mixpanel.track('Enlarged Deal', $(this).find('.destination-name').text());
-				 	$('.deal-box.large').removeClass('large');
-				 	$(this).addClass('large');
-				 	$('.deals-container').isotope('reLayout');
-			 	});			
-			}
-			
-		  },
-		  error: function(error) {
-		    alert("Error: " + error.code + " " + error.message);
-		  }
-		});
-
-	},
-	*/
 	getDeals: function() {
 		var Deals = Parse.Object.extend("Donor"),
 		deal = new Deals(),
@@ -87,36 +37,23 @@ $.extend(TRIP, {
 		query.find({
 		  success: function(results) {
 		  	$.each(results, function() {
-		  		console.log(this);
-		  		console.log(this.get("Accomplishments"));					
-
-				console.log(this.get("price")+this.get("imgUrl")+this.get("Accomplishments"));
-
-
-		  		//Compiling html with handlebars.js
-		  		console.log("handlebars");
 		  	var html = deal_template({
 					name: this.get("Name"), 
 					bg: this.get("imgUrl"),
 					description: this.get("Accomplishments"),
 					iq: this.get("IQ"),
-					//leaving: startDate.calendar(),
 					price: this.get("price"),
-					//old_price: this.get("price"),
-					//hotel_price: Math.round(hotelPrice),
 					total_price: this.get("price"),
-					buynowhref: this.get("imgUrl"),
-					//booknowhref: flight.get("hotel_link")
 			})
 			$('.deal-box').first().addClass('large');
 			$('.deals-container').isotope('insert', $(html));
 			TRIP.item_count++;
-			if (TRIP.item_count == TRIP.numCities) {
+			if (true) {
 				$('.deal-box').click(function(){
 				 	$('.deal-box.large').removeClass('large');
 				 	$(this).addClass('large');
 				 	$('.deals-container').isotope('reLayout');
-			 	});			
+			 	});	
 			}
 		  	});
 		  },
@@ -128,48 +65,7 @@ $.extend(TRIP, {
 });
  TRIP.getDeals();
 
-//mixpanel.track('Loaded Page');
-/*
-	function appendListing(html){
-	    var bound = $(html).click(function(){
-	      analytics.track("Enlarged Brick");
-	      //analytics.track("Enlarged Brick " + $(this).find('.title').text());
-	      $('.brick.large').removeClass('large');
-	      $(this).addClass('large');
-	      $('.brick-container').isotope('reLayout');
-	    })
-	    setTimeout(function(){
-	      $('.brick-container').isotope('insert', bound);
-	      $('.brick-container').isotope('reLayout')
-	      render();
-	    },0)
-	};
 
-	var ListingsQuery = new Parse.Query(Donor);
-	ListingsQuery._order = "-updatedAt";
-
-	var skip = 0;
-	var count = 40;
-	ListingsQuery.limit(count);
-
-	function loadListings(){
-	    ListingsQuery.find({ 
-	      success: function(results){
-	        $.each(results, function(i, item){
-	            var compiled = _.template($('.listing-template').html());
-	            html = compiled({
-	              l: item,
-	            })
-	            appendListing(html);
-	        })
-	        skip += count;
-	        ListingsQuery.skip(skip);
-	      }
-	    })    
-	  };
-*/
-
-  	//loadListings();
 
 
 });	
